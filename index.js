848a597b63280b5d9bc652d3f08ea707890f75f2
Clayton Hensley <chensley56@gmail.com>
2024-01-18 18:34:09 -0800

    added jest, updated shapes to classes, fixed text location

--- index.js
+++ index.js
@@ -1,39 +1,52 @@
 const fs = require('fs');
 const readline = require('readline');
-const shapes = require('./lib/shapes');
+const { Square, Circle, Triangle } = require('./lib/shapes');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-
 function generateSVG(chosenShape, shapeColor, text, textColor) {
-    const size = 100;
-  
-    if (shapes[chosenShape]) {
-      const shapeAttributes = shapes[chosenShape](size, shapeColor);
-  
-      const textX = 200 - text.length * 5; 
-      const textY = 200 + size / 2 + 10; 
-  
-      const svgContent = `
-        <svg width="400" height="400" xmlns="http://www.w3.org/2000/svg">
-          ${shapeAttributes}
-          <text x="${textX}" y="${textY}" fill="${textColor}" font-size="20" font-family="Arial">${text.slice(0, 3)}</text>
-        </svg>
-      `;
-  
-      fs.writeFileSync('output.svg', svgContent);
-  
-      console.log('SVG file generated successfully!');
-    } else {
+  const size = 100;
+  let shape, textX, textY;
+
+  switch (chosenShape.toLowerCase()) {
+    case 'square':
+      shape = new Square(size, shapeColor);
+      textX = 50 + size / 2 - text.length * 5;
+      textY = 50 + size / 2 + 5; 
+      break;
+    case 'circle':
+      shape = new Circle(size / 2, shapeColor); 
+      textX = 300 - text.length * 5;
+      textY = 300 + 5; 
+      break;
+    case 'triangle':
+      shape = new Triangle(size, shapeColor);
+      textX = 200 - text.length * 5;
+      textY = 150 + size / 4 + 5; 
+      break;
+    default:
       console.error('Invalid shape chosen.');
-    }
-  
-    rl.close();
+      rl.close();
+      return;
   }
-  
+
+  const shapeAttributes = shape.render();
+
+  const svgContent = `
+    <svg width="400" height="400" xmlns="http://www.w3.org/2000/svg">
+      ${shapeAttributes}
+      <text x="${textX}" y="${textY}" fill="${textColor}" font-size="20" font-family="Arial">${text.slice(0, 3)}</text>
+    </svg>
+  `;
+
+  fs.writeFileSync('output.svg', svgContent);
+
+  console.log('SVG file generated successfully!');
+  rl.close();
+}
 
 rl.question('Choose a shape (square, circle, triangle): ', (chosenShape) => {
   rl.question('Enter shape color (e.g., red, green, blue or #FF5733): ', (shapeColor) => {
